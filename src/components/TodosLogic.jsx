1c1b8001eb6ade8cf514d6efcf1ad8b83255e1c4
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-12 14:11:44 +0100

    Create static todos

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { flushSync } from 'react-dom';
 
 const TodosLogic = () => {
+    const todos = [
+        {
+            id: 1,
+            title: 'Setup Development Environment',
+            complete: true,
+        },
+        {
+            id: 2,
+            title: 'Develop Website and Add content',
+            complete: false,
+        },
+        {
+            id: 3,
+            title: 'Deploy to live server',
+            complete: false,
+        },
+    ]
   return (
-    <div>TodosLogic content</div>
+      <ul>
+          {todos.map((todo) => (
+              <li>{ todo.title }</li>
+          ))}
+    </ul>
   )
 }
 
