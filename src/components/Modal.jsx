ac94eb294e4eb615d3155f601a7726a6e37cfeb8
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-12 18:28:39 +0100

    Re-use custom hook on modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+import useOnClickOutside from "@/utils/useOnClickOutside";
+
+const Modal = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const ref = useRef();
+    useOnClickOutside(ref, openModal, () => setOpenModal(false));
+
+    return (
+        <div>
+            <button onClick={() => setOpenModal(true)}>Modal</button>
+            {openModal && (
+                <div ref={ref}>
+                    <span onClick={() => setOpenModal(false)}>X</span>
+                    <div>Modal content here</div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Modal;
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
 import Navbar from '@/components/Navbar';
+import Modal from './Modal';
 
 const TodoApp = () => {
     return (
         <>
             <Navbar />
+            <Modal />
             <Header />
             <TodosLogic />
         </>
