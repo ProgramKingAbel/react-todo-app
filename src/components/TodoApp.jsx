7ffda38613baacce3427c3a24504fe05db483a65
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-12 17:11:20 +0100

    Implement Navbar with state

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
+import Navbar from '@/components/Navbar';
 
 const TodoApp = () => {
     return (
         <>
+            <Navbar />
             <Header />
             <TodosLogic />
         </>
