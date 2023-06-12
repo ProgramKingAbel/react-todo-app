558f6e2809add9cb033933192c467fb06e852570
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-12 13:37:12 +0100

    Add support for absolute imports

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const TodoApp = () => {
-  return (
-      <div>
-          <h1>Hello World!</h1>
-          <p>Im a react Component</p>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>Im a react Component</p>
+        </div>
+    );
+};
 
-export default TodoApp
+export default TodoApp;
