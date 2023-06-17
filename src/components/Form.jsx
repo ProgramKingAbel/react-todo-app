3e99793b8e97df83ba55db961aa46cae6cb72ee1
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-17 22:11:13 +0100

    Add basic form

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+      <h1>React Form Handling</h1>
+      <form>
+        <label>
+          First Name: <input type="text" />
+        </label>
+      </form>
+    </>
+  )
+}
+
+export default Form
