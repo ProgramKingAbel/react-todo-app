fe7eb93bed0d220ce9d6815e273ea2f97afe6079
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-19 13:39:51 +0100

    Add onChange listener to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,12 +3,18 @@ import React, { useState } from 'react';
 const Form = () => {
   const [fname, setFname] = useState("");
 
+  const handler = (e) => {
+    setFname(e.target.value);
+  }
+
   return (
     <>
       <h1>Controlled Forms</h1>
       <form>
         <label>
-          First Name: <input type="text" value={fname} />
+          First Name: {" "}
+          
+          <input type="text" value={fname} onChange={handler}/>
         </label>
       </form>
       <h5>First name: { fname }</h5>
