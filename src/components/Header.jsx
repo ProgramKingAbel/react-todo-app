125e62393242fe4240ef186476c3b743c556d350
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-20 16:54:02 +0100

    Add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from '@/styles/Header.module.css';
 
 const Header = () => {
+  const headerStyle = {
+    padding: '20px 0',
+    lineHeight: '1.5em',
+    color: '#aeadad',
+    textAlign: 'center',
+  };
+
   return (
-      <header>
+       <header style={headerStyle} className={styles.header}>
           <h1>todos</h1>
           <p>Items will persisit in the browser localStorage</p>
       </header>
