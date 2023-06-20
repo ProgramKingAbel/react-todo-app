63ec953ab34ab7eb88dd199642ab284508192a8d
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-20 18:17:29 +0100

    Add edit mode

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from '@/styles/TodoItem.module.css';
 
 const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+
+
+
+    const [editing, setEditing] = useState(false);
+    const handleEditing = () => {
+        setEditing(true);
+    }
+
+    let viewMode = {};
+    let editMode = {};
+
+
+    if (editing) {
+        viewMode.display = 'none';
+    } else {
+        editMode.display = 'none';
+    }
 
     const completedStyle = {
         fontStyle: 'italic',
@@ -18,13 +35,22 @@ const TodoItem = ({ itemProp, handleChange, delTodo }) => {
                 type='checkbox'
                 checked={itemProp.completed}
                 onChange={()=> handleChange(itemProp.id)}
-            />
+                />
+                <button onClick={handleEditing}>Edit</button>
                 <button onClick={() => delTodo(itemProp.id)}>Delete</button>
                 <span style={itemProp.completed ? completedStyle : null}>
 
             {itemProp.title}
                 </span>
+
             </div>
+
+                <input
+      type="text"
+      value={itemProp.title}
+                className={styles.textInput}
+                style={editMode}
+    />
         </li>
     )
 }
