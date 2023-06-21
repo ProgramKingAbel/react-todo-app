f70a2598ebaacbdd53a8c30950e1522cb6ba50f2
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-21 01:02:34 +0100

    Create a shared Layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Layout = () => {
+  return (
+    <div className='wrapper'><Navbar /></div>
+  )
+}
+
+export default Layout
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -5,16 +5,19 @@ import About from '@/routes/About';
 import Login from '@/routes/Login';
 import Profile from '@/routes/Profile';
 import NotMatch from '@/routes/NotMatch';
+import Layout from '@/components/Layout';
 
 
 const TodoApp = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Layout />}> 
+            <Route index element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='login' element={<Login />} />
             <Route path='profile' element={<Profile />} />
             <Route path='*' element={ <NotMatch />} />
+            </Route>
 
         </Routes>
     )
